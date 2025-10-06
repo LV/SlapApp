296060fb4811afb137bba9cab13df43c49dfda33
Luis Victoria <v@luis.vi>
2025-10-06 19:15:36 -0400

    Initial Commit

---
+++ SlapApp/SlapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlapAppApp.swift
+//  SlapApp
+//
+//  Created by Luis Victoria on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SlapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SlapAppTests/SlapAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SlapAppTests.swift
+//  SlapAppTests
+//
+//  Created by Luis Victoria on 10/6/25.
+//
+
+import Testing
+@testable import SlapApp
+
+struct SlapAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
